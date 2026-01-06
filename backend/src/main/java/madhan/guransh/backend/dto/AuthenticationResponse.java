9d919286161d19e62326bc8464cd77ca234cebe0
Guransh Madhan <101924384+GuranshMadhan@users.noreply.github.com>
2026-01-06 04:38:47 +0000

    add: auth dtos

---
+++ backend/src/main/java/madhan/guransh/backend/dto/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package madhan.guransh.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token; // The JWT token
+}
