34c82febc58cb36043f686b2d799f43463bdba2f
Guransh Madhan <101924384+GuranshMadhan@users.noreply.github.com>
2026-01-09 04:56:37 +0000

    add: list questions from a global pool

--- backend/src/main/java/madhan/guransh/backend/repository/QuestionRepository.java
+++ backend/src/main/java/madhan/guransh/backend/repository/QuestionRepository.java
@@ -2,11 +2,17 @@ package madhan.guransh.backend.repository;
 
 import madhan.guransh.backend.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     Optional<Question> findById(Long id);
+
+    // List questions for infinite mode
+    @Query(value = "SELECT * FROM questions WHERE classroom_id IS NULL ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    List<Question> findRandomGlobalQuestions(int limit);
 
 }
