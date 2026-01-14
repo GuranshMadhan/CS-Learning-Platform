c3615baaf02643e4b6436a4675761481429ff854
Guransh Madhan <101924384+GuranshMadhan@users.noreply.github.com>
2026-01-14 23:59:37 +0000

    add: adding questions to quizzes

--- backend/src/main/java/madhan/guransh/backend/services/QuizService.java
+++ backend/src/main/java/madhan/guransh/backend/services/QuizService.java
@@ -1,10 +1,14 @@
 package madhan.guransh.backend.services;
 
 import lombok.RequiredArgsConstructor;
+import madhan.guransh.backend.enums.Difficulty;
+import madhan.guransh.backend.enums.QuestionType;
 import madhan.guransh.backend.model.Classroom;
+import madhan.guransh.backend.model.Question;
 import madhan.guransh.backend.model.Quiz;
 import madhan.guransh.backend.model.User;
 import madhan.guransh.backend.repository.ClassroomRepository;
+import madhan.guransh.backend.repository.QuestionRepository;
 import madhan.guransh.backend.repository.QuizRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +20,7 @@ public class QuizService {
 
     private final QuizRepository quizRepository;
     private final ClassroomRepository classroomRepository;
+    private final QuestionRepository questionRepository;
 
     public Quiz createQuiz(Long classroomId, String title, String description, User teacher) {
         // 1. Find the classroom
@@ -39,5 +44,28 @@ public class QuizService {
 
     public List<Quiz> getQuizzesForClassroom(Long classroomId) {
         return quizRepository.findAllByClassroomId(classroomId);
+    }
+
+    public Question addQuestionToQuiz(Long quizId, String content, String op1, String op2, String op3, String op4, String answer {
+        Quiz quiz = quizRepository.findById(quizId)
+                .orElseThrow(() -> new RuntimeException("Quiz not found"));
+
+        Question question = new Question();
+        question.setContent(content);
+        question.setOption1(op1);
+        question.setOption2(op2);
+        question.setOption3(op3);
+        question.setOption4(op4);
+        question.setCorrectAnswer(answer);
+
+        question.setXpValue(10);
+        question.setType(QuestionType.MULTIPLE_CHOICE);
+        question.setDifficulty(Difficulty.EASY);
+
+        question.setQuiz(quiz);
+
+        questionRepository.save(question);
+
+        return questionRepository.save(question);
     }
 }
