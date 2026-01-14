e5d3c8a68514db2360ba3551d3b54a826049bdd6
Guransh Madhan <101924384+GuranshMadhan@users.noreply.github.com>
2026-01-14 23:53:45 +0000

    Create QuizRepository.java

---
+++ backend/src/main/java/madhan/guransh/backend/repository/QuizRepository.java
@@ -0,0 +1,11 @@
+package madhan.guransh.backend.repository;
+
+import madhan.guransh.backend.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    // We need to find all quizzes belonging to a specific classroom
+    List<Quiz> findAllByClassroomId(Long classroomId);
+}
